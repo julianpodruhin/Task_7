29b5f510e72c68e5748198c3dc818dc887c6325c
yulianpodruhin <yulianpodruhin@mail.ru>
2023-03-04 17:18:54 +0300

    Lab 7

--- src/Airplane.java
+++ src/Airplane.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Airplane {
+
+public class Airplane {
+
+    public class Wing {
+        public double weight;
+
+        public double getWeight() {
+            return weight;
+        }
+
+        public Wing(double weight) {
+            this.weight = weight;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Airplane airplane = new Airplane();
+        Airplane.Wing wing1 = airplane.new Wing(110);
+        Airplane.Wing wing2 = airplane.new Wing(100);
+
+        System.out.println("Крыло 1. Вес - " + wing1.getWeight());
+        System.out.println("Крыло 2. Вес - " + wing2.getWeight());
     }
 }
